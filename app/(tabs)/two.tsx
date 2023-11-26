9b071f38e911bd50b17c08ffe0f9b220cd18380d
Ivan Kotov <iamivankotov@gmail.com>
2023-11-26 02:47:11 +0100

    map

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,20 +1,97 @@
+import React from 'react';
+import { View } from 'react-native';
 import { StyleSheet } from 'react-native';
+import MapView, {Circle, Geojson, Polyline} from 'react-native-maps';
+import { FeatureCollection, Geometry } from 'geojson';
+import stationData from '../../assets/stations.json';
+import {Station,} from '../../types';
 
-import { View } from '../../components/Themed';
-import {useState} from 'react';
-import Map from '../../components/Map';
-import SlidingPanel from '../../components/SlidingPanel';
+
+const stationColors: any = {
+  'L1': 'red',
+  'L2': 'purple',
+  'L3': 'green',
+  'L4': 'yellow',
+  'L5': 'blue',
+  'L9S': 'orange',
+  'L9N': 'orange',
+  'L10N': 'aquamarine',
+  'L10S': 'aquamarine',
+  'L11': 'lightgreen',
+  'FM': 'darkgreen',
+};
+const getColorByPicto = (picto: string): string => {
+  return stationColors[picto] || 'gray';
+};
 
 export default function TabTwoScreen() {
-  const [destination, setDestination] = useState({from: '', to: ''});
+  // const [destination, setDestination] = useState({from: '', to: ''});
+  const stations: Station[] = stationData.features.map(feature => ({
+    id: feature.id,
+    name: feature.properties.NOM_ESTACIO,
+    latitude: feature.geometry.coordinates[1],
+    longitude: feature.geometry.coordinates[0],
+    picto: feature.properties.PICTO,
+  }));
+  const lines: { [key: string]: Station[] } = {};
+  stations.forEach(station => {
+    const line = station.picto;
+    if (!lines[line]) {
+      lines[line] = [];
+    }
+    lines[line].push(station);
+  });
+  const myPlace: FeatureCollection<Geometry> = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Point',
+          coordinates: [64.165329, 48.844287],
+        }
+      }
+    ]
+  };
 
   return (
     <View style={styles.container}>
-      <Map destination={destination} setDestination={setDestination} />
-      <SlidingPanel
-        destination={destination}
-        setDestination={setDestination}
-      />
+      <MapView style={styles.map}
+         showsUserLocation={true}
+         zoomEnabled={true}
+         zoomTapEnabled={true}
+         initialRegion={{
+           latitude: 41.3851,
+           longitude: 2.1734,
+           latitudeDelta: 0.0922,
+           longitudeDelta: 0.0421,
+         }}
+      >
+        <Geojson
+          geojson={myPlace}
+          strokeColor="red"
+          fillColor="green"
+          strokeWidth={2}
+        />
+        {Object.keys(lines).map(line => (
+          <Polyline
+            key={line}
+            coordinates={lines[line].map(({ latitude, longitude }) => ({ latitude, longitude }))}
+            strokeColor={getColorByPicto(line)}
+            strokeWidth={2}
+          />
+        ))}
+        {stations.map(station => (
+          <Circle
+            key={station.id}
+            center={{ latitude: station.latitude, longitude: station.longitude }}
+            radius={15}
+            fillColor={getColorByPicto(station.picto)}
+            // strokeWidth={0}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
@@ -25,13 +102,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  map: {
+    width: '100%',
+    height: '100%',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  customMarker: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: 'green',
   },
 });
